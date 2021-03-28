a4fd817b16242121b9488d331011ce5411b403c6
Linda Nduhiu <lwn3@cornell.edu>
2021-03-28 02:01:23 -0400

    insta2

--- app/src/main/java/com/example/instagram/Post.java
+++ app/src/main/java/com/example/instagram/Post.java
@@ -5,16 +5,12 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
-import java.io.File;
-import java.security.Key;
-
-
 @ParseClassName("Post")
 public class Post extends ParseObject {
-
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_KEY = "createdAt";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -28,37 +24,14 @@ public class Post extends ParseObject {
         return getParseFile(KEY_IMAGE);
     }
 
-    public void setImage(ParseFile image){
-        put(KEY_IMAGE, image);
+    public void setImage(ParseFile parseFile) {
+        put(KEY_IMAGE, parseFile);
     }
 
-    // Ensure that your subclass has a public default constructor
-    public Post() {
-        super();
-    }
-
-//    // Add a constructor that contains core properties
-//    public Post(String body) {
-//        super();
-//        setBody(body);
-//    }
-
-//    // Use getString and others to access fields
-//    public String getBody() {
-//        return getString("body");
-//    }
-
-//    // Use put to modify field values
-//    public void setBody(String value) {
-//        put("body", value);
-//    }
-
-    // Get the user for this item
     public ParseUser getUser() {
         return getParseUser(KEY_USER);
     }
 
-    // Associate each item with a user
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
     }
